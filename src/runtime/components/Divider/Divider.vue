ad6184d82a8614f4d2b0a547c8a473dfde7c0172
zaifer <zaif3r@pm.me>
2022-12-16 22:09:14 -0500

    create Divider component

---
+++ src/runtime/components/Divider/Divider.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="divider" :class="{ 'divider-horizontal': horizontal }">
+        <slot />
+    </div>
+</template>
+<script lang="ts" setup>
+defineProps<{
+    horizontal?: boolean;
+}>();
+</script>
--- src/runtime/components/index.ts
+++ src/runtime/components/index.ts
@@ -1,6 +1,7 @@
 export { default as Col } from "./Col";
 export { default as Row } from "./Row";
 export { default as Wrap } from "./Wrap";
+export { default as Divider } from "./Divider";
 
 export { default as Navbar } from "./Navbar";
 export { default as Footer } from "./Footer";
