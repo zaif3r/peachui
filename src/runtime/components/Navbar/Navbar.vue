3cf611442aaaeec15f988039dffd7cd6ba4c390d
zaifer <zaif3r@pm.me>
2022-12-16 19:24:12 -0500

    create navbar component

---
+++ src/runtime/components/Navbar/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav class="navbar">
+        <div class="navbar-start gap-1">
+            <slot name="start" />
+            <slot />
+        </div>
+        <div class="navbar-center gap-1">
+            <slot name="center" />
+        </div>
+        <div class="navbar-end gap-1">
+            <slot name="end" />
+        </div>
+    </nav>
+</template>
--- src/runtime/components/index.ts
+++ src/runtime/components/index.ts
@@ -2,5 +2,7 @@ export { default as Col } from "./Col";
 export { default as Row } from "./Row";
 export { default as Wrap } from "./Wrap";
 
+export { default as Navbar } from "./Navbar";
+
 export { default as Button } from "./Button";
 export { default as Badge } from "./Badge";
