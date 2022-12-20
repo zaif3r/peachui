04e4151384d90a45fb7a042372df36d0f8adb350
zaifer <zaif3r@pm.me>
2022-12-20 00:14:30 -0500

    rename slots

--- src/runtime/components/Label/Label.vue
+++ src/runtime/components/Label/Label.vue
@@ -1,9 +1,9 @@
 <template>
     <label :for="props.for" class="label">
         <span class="label-text" v-bind="$attrs">
-            <slot name="text"> {{ text }} </slot>
+            <slot> {{ text }} </slot>
         </span>
-        <slot />
+        <slot name="content" />
         <span v-if="alt || $slots.alt" class="label-text-alt" v-bind="$attrs">
             <slot name="alt">
                 {{ alt }}
