4f408a58fb78d2c5b55cc08e0c4130283d8dfac6
zaifer <zaif3r@pm.me>
2023-01-18 08:57:26 -0500

    fix: remove default w-full class for form children

--- src/runtime/components/Form/Form.vue
+++ src/runtime/components/Form/Form.vue
@@ -3,16 +3,3 @@
         <slot />
     </div>
 </template>
-<style>
-.form-control {
-    @apply w-full;
-}
-.form-control > div,
-.form-control .input,
-.form-control .textarea,
-.form-control .file-input,
-.form-control .label,
-.form-control .select {
-    @apply w-full;
-}
-</style>
